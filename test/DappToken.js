2a63c1ee666efebf03e1f235e1dce9b52cbc161f
josephtascona <joseph.tascona@gmail.com>
2019-02-06 16:36:36 -0500

    4. Token Sale

--- test/DappToken.js
+++ test/DappToken.js
@@ -14,7 +14,7 @@ contract('DappToken', function(accounts) {
       assert.equal(symbol, 'DAPP', 'has the correct symbol');
       return tokenInstance.standard();
     }).then(function(standard) {
-      assert.equal(standard, 'DApp Token v1.0', 'has the correct standard')
+      assert.equal(standard, 'DApp Token v1.0', 'has the correct standard');
     });
   })
 
@@ -66,12 +66,12 @@ contract('DappToken', function(accounts) {
     }).then(function(receipt) {
       assert.equal(receipt.logs.length, 1, 'triggers one event');
       assert.equal(receipt.logs[0].event, 'Approval', 'should be the "Approval" event');
-      assert.equal(receipt.logs[0].args._owner, accounts[0], 'logs the account the tokens are transferred by');
-      assert.equal(receipt.logs[0].args._spender, accounts[1], 'logs the account the tokens are transferred to');
+      assert.equal(receipt.logs[0].args._owner, accounts[0], 'logs the account the tokens are authorized by');
+      assert.equal(receipt.logs[0].args._spender, accounts[1], 'logs the account the tokens are authorized to');
       assert.equal(receipt.logs[0].args._value, 100, 'logs the transfer amount');
       return tokenInstance.allowance(accounts[0], accounts[1]);
     }).then(function(allowance) {
-      assert.equal(allowance.toNumber(), 100, 'stores the allowance for delegated transfer');
+      assert.equal(allowance.toNumber(), 100, 'stores the allowance for delegated trasnfer');
     });
   });
 
@@ -84,8 +84,8 @@ contract('DappToken', function(accounts) {
       // Transfer some tokens to fromAccount
       return tokenInstance.transfer(fromAccount, 100, { from: accounts[0] });
     }).then(function(receipt) {
-      // Approve spendingAccount to spend 10 tokens from fromAccount
-      return tokenInstance.approve(spendingAccount, 10, { from: fromAccount })
+      // Approve spendingAccount to spend 10 tokens form fromAccount
+      return tokenInstance.approve(spendingAccount, 10, { from: fromAccount });
     }).then(function(receipt) {
       // Try transferring something larger than the sender's balance
       return tokenInstance.transferFrom(fromAccount, toAccount, 9999, { from: spendingAccount });
@@ -102,7 +102,7 @@ contract('DappToken', function(accounts) {
     }).then(function(receipt) {
       assert.equal(receipt.logs.length, 1, 'triggers one event');
       assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
-      assert.equal(receipt.logs[0].args._from, fromAccount, 'logs the account the tokens are transferred by');
+      assert.equal(receipt.logs[0].args._from, fromAccount, 'logs the account the tokens are transferred from');
       assert.equal(receipt.logs[0].args._to, toAccount, 'logs the account the tokens are transferred to');
       assert.equal(receipt.logs[0].args._value, 10, 'logs the transfer amount');
       return tokenInstance.balanceOf(fromAccount);
